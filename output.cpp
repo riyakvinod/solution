a2ec01bee31962d4cbce836442fe161b81092e29
riyakvinod <56680295+riyakvinod@users.noreply.github.com>
2022-03-23 14:04:17 +0530

    Add files via upload

---
+++ output.cpp
@@ -0,0 +1,8 @@
+Enter The Number Of Employee
+4
+Here the goodies that are selected for distribution are
+Fitbit Plus : 7980
+IPods : 22349
+MIBand : 999
+Cult Pass : 2799
+And the difference between the chosen goodie with highest price and the lowest price is 21350
